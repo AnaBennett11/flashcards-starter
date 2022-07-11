4da929d516df810473b57b734de76dd31e50503a
Ana Bennett <anabennett1111@gmail.com>
2022-07-11 14:20:23 -0500

    Invoked the startGame function in the index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const Game = require('./src/Game');
+
+let game = new Game();
+console.log('Your project is running...');
+game.startGame();
 // This is where your project starts.
 
-console.log('Your project is running...'); 
+
